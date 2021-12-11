4798230c272b3de662c3d24ccb00fc1a039e2c3a
anasbelmokhtar <anasbelmokhtar@gmail.com>
2021-12-11 18:41:00 -0500

    Nav menu works and displays

--- app/src/main/java/com/example/final_project/DashboardActivity.java
+++ app/src/main/java/com/example/final_project/DashboardActivity.java
@@ -6,7 +6,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -22,14 +24,12 @@ public class DashboardActivity extends AppCompatActivity {
     private NavigationView nv;
     Toolbar tb;
 
-    protected void onCreate( Bundle savedInstanceState ) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
         // assigning ID of the toolbar to a variable
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        MenuInflater menuInflater = getMenuInflater();
 
         // using toolbar as ActionBar
         setSupportActionBar(toolbar);
@@ -44,10 +44,31 @@ public class DashboardActivity extends AppCompatActivity {
         //creating an object of the MenuInflater to inflate the menu
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.nav_menu, menu);
-       //  menuInflater.inflate(R.menu.toolbar_menu, menu);
-
-
+        //  menuInflater.inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //Using a switch to determine which item was clicked and then perfomrming a desired action
+        switch (item.getItemId()) {
+
+            //handle item1 click event
+            case R.id.search:
+                Toast.makeText(this, "you clicked on Search", Toast.LENGTH_SHORT).show();
+                return true;
+
+            //handle item2 click event
+            case R.id.logout:
+                setResult(500);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
 }
